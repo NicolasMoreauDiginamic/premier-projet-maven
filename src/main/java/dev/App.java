64c73c8a903f17199a6973770538161ec7b0432d
Nicolas Moreau <nicolas.moreau93@orange.fr>
2021-12-20 15:19:38 +0100

    commit integration librairie

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import com.github.lalyos.jfiglet.*;
 /**
  * Hello world!
  *
@@ -8,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String asciiArt = FigletFont.convertOneLine("Hello World!");
+        System.out.println(asciiArt);
     }
 }
